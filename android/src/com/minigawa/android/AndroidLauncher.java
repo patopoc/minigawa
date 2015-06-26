80b5c8d8eadbf8871608114060895c8c11f77ec7
steve <admin@mail.com>
2015-06-26 14:27:16 -0400

    Starting Nano Engine

---
+++ android/src/com/minigawa/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.minigawa.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.minigawa.GameConsole;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameConsole(), config);
+	}
+}
---
+++ desktop/src/com/minigawa/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.minigawa.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.minigawa.GameConsole;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameConsole(), config);
+	}
+}
